a2a5013fb8cd7602ac7c9c49b170d335b55867ec
DelanoKano <2042653@talnet.nl>
2024-04-25 14:07:00 +0200

    update

---
+++ myCV-Website/script.js
@@ -0,0 +1,17 @@
+const expElement = document.getElementById('exp');
+const aboutElement = document.getElementById('aboutyou');
+const workElement = document.getElementById('iwork');
+const inputElement = document.getElementById('c6');
+
+window.addEventListener('load', () => {
+    document.getElementById('c5').checked = true;  
+});
+
+window.addEventListener('load', () => {
+    document.getElementById('c1').checked = true;    
+});
+
+window.addEventListener('load', () => {
+    document.getElementById('c6').checked = true;    
+});
+
