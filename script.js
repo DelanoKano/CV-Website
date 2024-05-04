a2f7295f1878090328c5e319ee8cf5f69d1bebea
DelanoKano <2042653@talnet.nl>
2024-05-04 23:01:29 +0200

    update javascript and cleaned my code

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-const expElement = document.getElementById('exp');
-const aboutElement = document.getElementById('aboutyou');
-const workElement = document.getElementById('iwork');
-const inputElement = document.getElementById('c6');
-
-window.addEventListener('load', () => {
-    document.getElementById('c5').checked = true;  
+window.addEventListener("load", () => {
+  document.getElementById("c5").checked = true;
 });
 
-window.addEventListener('load', () => {
-    document.getElementById('c1').checked = true;    
+window.addEventListener("load", () => {
+  document.getElementById("c1").checked = true;
 });
 
-window.addEventListener('load', () => {
-    document.getElementById('c6').checked = true;    
+window.addEventListener("load", () => {
+  document.getElementById("c6").checked = true;
 });
-
